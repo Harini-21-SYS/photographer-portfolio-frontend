06af9c55f49cece5c96274a95d93959ffa1b9743
Harini <hariniragunathan06@gmail.com>
2026-07-07 19:26:40 +0530

    feat:log out feature added

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ AOS.init({
     duration:1000,
     once:false,
 });
+const userName = localStorage.getItem("userName");
+const userEmail = localStorage.getItem("userEmail");
+
+if (userName) {
+
+    document.getElementById("profileContainer").style.display = "block";
+
+    document.getElementById("userName").textContent = userName;
+
+    document.getElementById("profileName").textContent = userName;
+
+    document.getElementById("profileEmail").textContent = userEmail;
+}
 // Auto update copyright year
 // document.getElementById("year").textContent = new Date().getFullYear();
 const bookBtn=document.getElementById("bookBtn");
